e3a497e53cb6c148a53d08b97c8f2d79659d5db8
Christoph Kepler <christoph.kepler@arts.aero>
2014-04-02 11:46:47 +0200

    Outsourced another approach

---
+++ htdocs/scripts/another_approach.js
@@ -0,0 +1,26 @@
+/**
+ * @author Christoph Kepler
+ */
+
+/**
+ * Another Approach if necessary
+ */ 
+// function another_approach(before)  {
+	// linenumber = before + 1;
+	// remove_and_animate_values(linenumber);
+// }
+//
+// function insert_values() {
+	// for (i=1; i==7; i++) {
+		// inverted_i = 7 - i;
+		// that = $('div#content div:nth-of-type(' + i + ')');
+		// that.after('<div class="flipline flipline-' + i + '">' + get_array_entry(inverted_i,offset) + '</div>');
+	// }
+// }
+// 
+// function remove_and_animate_values(linenumber) {
+		// that = $('div#content div:nth-of-type(' + linenumber + ')');
+		// that.remove();
+		// that = $('div#content div:nth-of-type(' + linenumber + ')');
+		// animate_line(that);
+// }
