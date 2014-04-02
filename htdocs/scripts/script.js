7a16eec549e77bdcefdb7bd7b14c2a21f00c559f
Christoph Kepler <christoph.kepler@arts.aero>
2014-04-02 11:47:01 +0200

    String Normalizing

--- htdocs/scripts/script.js
+++ htdocs/scripts/script.js
@@ -1,8 +1,23 @@
 /**
  * @author Christoph Kepler
+ * 
+ * 
+ * Slipt-Flap Config
+ * .splitFlap({
+ *	'image' : 'css/images/chars.png',
+ * 	'charsMap' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789.,!?#@()+-=',
+ * 	'charWidth' : 50,	
+ * 	'charHeight' : 100,
+ * 	'padDir' : 'left',
+ * 	'padChar' : ' ',
+ * 	'speed' : 3,
+ * 	'speedVariation' : 2
+ * });
  */
 
 var nid = [
+	'testtesttesttesttesttesttesttesttesttesttest',
+	'testtesttesttesttesttestte',
 	'Stelle  1',
 	'Stelle  2',
 	'Stelle  3',
@@ -48,36 +63,64 @@ function get_array_size(array) {
 	array_length = array.length;
 	return array_length;
 }
+
+function normalize_entries(entry) {
+	entry_length = entry.length;
+	if(entry_length < 26) {
+		diff = 26 - entry_length;
+		for(i = 1;i <= diff; i++) {
+			entry = entry + ' ';
+		}
+		return entry;
+	}
+	else if(entry_length > 26) {
+    entry = entry.substring(0,23)+"...";
+		return entry;
+	}
+	else {
+		return entry;
+	}
+}
+
 function get_array_entry(linenumber) {
 	entrynumber = offset - linenumber;
 	if(entrynumber < 0) {
 		entrynumber = get_array_size(nid) + entrynumber;
 	}
 	entry = nid[entrynumber];
+	entry = normalize_entries(entry);
 	return entry;
+}
+
+function first_line(linenumber) {
+	that = $('div#content>div:nth-of-type(' + linenumber + ')');
+	that.remove();
+	that = null;
+	that = $('div#content>div:nth-of-type(' + linenumber + ')');
+	that.before('<div id="line-1" class="flipline">' + get_array_entry(linenumber) + '</div>');
+	that =  null;
+	that = $('div#content>div:nth-of-type(' + linenumber + ')');
+	animate_line(that);
+	that = null;
+}
+
+function else_lines(linenumber,before) {
+	that = $('div#content>div:nth-of-type(' + linenumber + ')');
+	that.remove();
+	that = null;
+	$('div#content>div:nth-of-type(' + before + ')').after('<div id="line-' + linenumber + '" class="flipline">' + get_array_entry(linenumber) + '</div>');
+	that = $('div#content>div:nth-of-type(' + linenumber + ')');
+	animate_line(that);
+	that = null;
 }
 
 function flipper(before) {
 	linenumber = before + 1;
 	if(linenumber == 1) {
-		that = $('div#content>div:nth-of-type(' + linenumber + ')');
-		that.remove();
-		that = null;
-		that = $('div#content>div:nth-of-type(' + linenumber + ')');
-		that.before('<div id="line-1" class="flipline">' + get_array_entry(linenumber) + '</div>');
-		that =  null;
-		that = $('div#content>div:nth-of-type(' + linenumber + ')');
-		animate_line(that);
-		that = null;
+		first_line(linenumber);
 	}
 	else {
-		that = $('div#content>div:nth-of-type(' + linenumber + ')');
-		that.remove();
-		that = null;
-		$('div#content>div:nth-of-type(' + before + ')').after('<div id="line-' + linenumber + '" class="flipline">' + get_array_entry(linenumber) + '</div>');
-		that = $('div#content>div:nth-of-type(' + linenumber + ')');
-		animate_line(that);
-		that = null;
+		else_lines(linenumber,before);
 	}
 }
 
@@ -91,7 +134,7 @@ function everybodyDotheFlop() {
 				set_offset();
    			clearInterval(flipperintervall);
   		}
-		},500);
+		},1500);
 }
 
 function bypassintervall() {
@@ -104,30 +147,14 @@ function init() {
 	}, 15000);
 }
 
+function create_flipper_material() {
+	content = $('#content').html();
+	$('#content').html(content + '<div id="line-1" class="flipline"><!-- Init --></div>\n<div id="line-2" class="flipline"><!-- Init --></div>');
+	content = null;
+}
+
 $(document).ready(function() {
+	create_flipper_material();
 	bypassintervall();
 	init();
 })
-
-/**
- * Another Approach if necessary
- */ 
-// function another_approach(before)  {
-	// linenumber = before + 1;
-	// remove_and_animate_values(linenumber);
-// }
-//
-// function insert_values() {
-	// for (i=1; i==7; i++) {
-		// inverted_i = 7 - i;
-		// that = $('div#content div:nth-of-type(' + i + ')');
-		// that.after('<div class="flipline flipline-' + i + '">' + get_array_entry(inverted_i,offset) + '</div>');
-	// }
-// }
-// 
-// function remove_and_animate_values(linenumber) {
-		// that = $('div#content div:nth-of-type(' + linenumber + ')');
-		// that.remove();
-		// that = $('div#content div:nth-of-type(' + linenumber + ')');
-		// animate_line(that);
-// }
