c57508a7d08c9e099a2a1f54744c5ae63c6f18bd
Christoph Kepler <christoph.kepler@arts.aero>
2014-04-30 12:22:39 +0200

    Made the flipper more configurable interval and linenumbers renamed js file

--- htdocs/scripts/flipboard.js
+++ htdocs/scripts/flipboard.js
@@ -16,55 +16,95 @@
  * 
  */
 
+
+/**
+ * Configuration of the flipper and intervall and shit. ^^
+ */
 var nid = [
-	['testtesttesttesttesttesttesttesttesttesttest','b','g'],
-	['testtesttesttesttesttestte','a','h'],
-	['Stelle  1','f','d'],
-	['Stelle  2','c','c'],
-	['Stelle  3','a','h'],
-	['Stelle  4','g','d'],
-	['Stelle  5','d','f'],
-	['Stelle  6','e','a'],
-	['Stelle  7','h','b'],
-	['Stelle  8','d','c'],
-	['Stelle  9','b','g'],
-	['Stelle 10','h','g'],
-	['Stelle 11','a','a'],
-	['Stelle 12','b','b'],
-	['Stelle 13','a','h'],
-	['Stelle 14','f','d'],
-	['Stelle 15','d','e'],
-	['Stelle 16','g','e'],
-	['Stelle 17','g','f'],
-	['Stelle 18','h','a'],
-	['Stelle 19','b','c']
-];
+	['INGENIEUR FUER VERFAHRENSTECHNIK','A','KOUR'],
+	['PRAKTIKANT TECHNICAL SALES','B','DUS'],
+	['WERKSTUDENT ENGINEERING: CABIN + CARGO','C','HAM'],
+	['PRAKTIKUM IM BEREICH LEAN LOGISTICS','C','HAM'],
+	['WERKSTUDENT: MATERIALIEN UND PROZESSE','C','BRE'],
+	['ABSCHLUSSARBEIT: A400M FLUEGELMONTAGE','C','STAD'],
+	['PRAKTIKUM: SPACE EXPLORATION','D','BRE'],
+	['PRAKTIKUM: SIMUL. HINDERNISWARNSYSTEM','D','FDH'],
+	['PRAKTIKUM: HIGH PERFORMANCE COMPUTING','D','ULM'],
+	['FINAL THESIS: HIGH VOLTAGE DC NETWORK','E','HAM'],
+	['DOKTORARBEIT: MIKROWELLENTECHNOLOGIEN','E','MUC'],
+	['VIE: HELICOPTER TRAINING SIMULATION','F','MUC'],
+	['PRAKTIKUM PROJEKTMANAGEMENT TIGER','F','DONW'],
+	['PRAKTIKUM SIMULATION ? SOFTWAREENTW.','F','MAN'],
+	['PROJEKTINGENIEURE AERODYNAMIK','G','BUND'],
+	['AUSBILDUNG ZUM INDUSTRIEMECHANIKER','H','WILD'],
+	['JUNIOR VBA ENTWICKLER IN DER LUFTFAHRT','I','MAN'],
+	['QUALIFIZIERUNG FLUGGERAETMECHANIKER','I','HAM'],
+	['FACHHELFER LOGISTIK','I','DONW'],
+	['PRAKTIKANT?DIPLOMAND AEROSPACE','J','MUC'],
+	['PRAKTIKANT?IN SALES + MARKETING','K','FRA'],
+	['JUNIOR SUPPLY CHAIN MANAGER ','L','HAM'],
+	['DIPLOM-INGENIEUR LUFT- UND RAUMFAHRT','M','DRS'],
+	['AUSBILDUNG TECHNISCHER PRODUKTDESIGNER','N','GERS'],
+	['MINT: SPITZENFORSCH. ENERGIE + VERKEHR','O','BUND'],
+	['WERKSTUDENT IM MANAGEMENT CONSULTING','P','BER'],
+	['STUDENT. AUSHILFE KUNSTSTOFFFERTIGUNG','Q','DRS'],
+	['PRAKTIKUM PROGRAMMMANAGEMEMENT A330 ','Q','DRS'],
+	['BERUFSEINSTIEG KONSTRUKTIONSINGENIEUR','R','FDH'],
+	['AUSBILDUNG TECHNISCHER ZEICHNER','S','BLAN'],
+	['PRAKTIKUM: BETREUUNG FLUGSIMULATOREN','T','FRA'],
+	['PRAKTIKANT INFLIGHT + CATERING','U','HAM'],
+	['HIWI IM FORSCHUNGSPROJEKT AIRPORT2030','V','HAM'],
+	['WISS. MITARBEITER SATELLITENTECHNIK','W','BER'],
+	['PRAKTIKANT?IN IM BEREICH LOGISTIK','X','BUND'],
+	['WERKSTUDENT: SATELLITENKOMMUNIKATION','Y','MUC'],
+	['PRAKTIKUM UNTERNEHMENSENTWICKLUNG','Z','MUC'],
+	['PRAKTIKUM IN DER PRODUKTION','Z','MUC'],
+	['PROMOTIONSSTIPENDIUM:  TRAEGERRAKETEN','0','MUC'],
+	['PRAKTIKANT ARBEITSVORBEREITUNG ','1','ARNS'],
+	['TRAINEEPROGRAMM LUFTFAHRT KONSTRUKTION','2','BRE'],
+	['PRAKTIKANT IM BEREICH QM SYSTEMS','3','NOR'],
+	['PRAKTIKANT?BACHELOR TEILEFERTIGUNG','3','NOR'],
+	['DIPLOM-, MASTER- ODER BACHELORARBEIT','4','FRA'],
+	['WEITERBD. SERVICEFACHKRAFT LUFTVERKEHR','5','BER'],
+	['INTERN: ALLIANCE + NETWORK DEVELOPMENT','6','FRA'],
+	['PRAXISSTUDENT DESIGN FOR MANUFACTURING','7','BACK'],
+	['PRAKT. RAUMFAHRT: MECHANISCHE EFFEKTE ','7','BACK'],
+	['LUFTVERKEHRSKAUFMANN?-FRAU BERUFSBEGL.','8','BER'],
+	['FORTBILDUNG AIRCRAFT MAINTEN. MECHANIC','8','BER'],
+]
 
 var flipperinterval;
-var offset = 7;
+var linenumbers = 12;
+var offset = linenumbers;
+var lineflipintervall = 7;
+var completeflipintervall = 90;
+
+function makemilliseconds(seconds) {
+	return seconds * 1000;
+}
 
 function animate_line(animate_selector) {
 	title = $(animate_selector + ' .title');
 	title.splitFlap({
-	'image' : 'css/images/chars.png',
-		'speed' : 10,
-		'speedVariation' : 0
+		'image' : 'css/images/chars.png',
+		'charsMap' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789.,?@():+-',
+		'speed' : 5,
+		'speedVariation' : 2
 	});
 	title = null;
 	logo = $(animate_selector + ' .logo');
 	logo.splitFlap({
 		'image' : 'css/images/logos.png',
 		'charWidth' : 200,
-		'charsMap' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-		'speed' : 8,
+		'charsMap' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012345678',
+		'speed' : 3,
 		'speedVariation' : 2
 	});
 	logo = null;
 	locationcode = $(animate_selector + ' .location');
 	locationcode.splitFlap({
 		'image' : 'css/images/chars.png',
-		'charWidth' : 200,
-		'charsMap' : 'ABCDEFGH',
+		'charsMap' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ',
 		'speed' : 3,
 		'speedVariation' : 2
 	});
@@ -85,17 +125,17 @@ function get_array_size(array) {
 	return array_length;
 }
 
-function normalize_entries(entry) {
+function normalize_entries(entry,length) {
 	entry_length = entry.length;
-	if(entry_length < 26) {
-		diff = 26 - entry_length;
+	if(entry_length < length) {
+		diff = length - entry_length;
 		for(i = 1;i <= diff; i++) {
 			entry = entry + ' ';
 		}
 		return entry;
 	}
-	else if(entry_length > 26) {
-    entry = entry.substring(0,25)+"@";
+	else if(entry_length > length) {
+    entry = entry.substring(0,length-1)+"@";
 		return entry;
 	}
 	else {
@@ -111,13 +151,14 @@ function get_array_entry(linenumber,type) {
 	switch(type) {
 		case 'title':
 			entry = nid[entrynumber][0];
-			entry = normalize_entries(entry);
+			entry = normalize_entries(entry,40);
 			break;
 		case 'logo':
 			entry = nid[entrynumber][1];
 			break;
 		case 'location':
 			entry = nid[entrynumber][2];
+			entry = normalize_entries(entry,4);
 			break;
 		default:
 			alert('This should not happen!!1!!11!einself');
@@ -175,11 +216,11 @@ function everybodyDotheFlop() {
 		setInterval(function() {
 			flipper(counter);
 			counter++;
-			if(counter === 7) {
+			if(counter === linenumbers) {
 				set_offset();
    			clearInterval(flipperintervall);
   		}
-		},2000);
+		}, makemilliseconds(lineflipintervall));
 }
 
 function bypassintervall() {
@@ -189,7 +230,7 @@ function bypassintervall() {
 function init() {
 	window.setInterval(function() {
 		everybodyDotheFlop();
-	}, 45000);
+	}, makemilliseconds(completeflipintervall));
 }
 
 function create_flipper_material() {
